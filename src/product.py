4104fc9e7d65d4691a0ced2ae853ebee007c689d
USER <USER@example.com>
2025-07-02 15:58:18 +0300

    zadanie 1, add class

---
+++ src/product.py
@@ -0,0 +1,22 @@
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    name: str
+    description: str
+    products: str
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
